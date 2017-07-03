611e1918b4d2effb3d475eca237cbeefbfcd97d9
Ebony <angelic2_626829@yahoo.com>
2017-07-03 13:43:37 +0000

    Ready to deploy.  Using postgresql for production

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,23 +1,34 @@
 class Location < ActiveRecord::Base
-    geocoded_by :full_street_address
-    after_validation :geocode
-    
-    def full_street_address
-        "#{street_address}, #{city}, GA"
-    end 
+  geocoded_by :full_street_address
+  after_validation :geocode
+
+#   MARTA_CITIES = ['Atlanta',
+#                   'Alpharetta',
+#                   'Avondale Estates',
+#                   'Chamblee',
+#                   'Clarkston',
+#                   'College Park',
+#                   'Decatur',
+#                   'Doraville',
+#                   'Dunwoody',
+#                   'East Point',
+#                   'Ellenwood',
+#                   'Fairburn',
+#                   'Forest Park',
+#                   'Hapeville',
+#                   'Jonesboro',
+#                   'Lake City',
+#                   'Lovejoy',
+#                   'Lithonia',
+#                   'Morrow',
+#                   'Palmetto',
+#                   'Riverdale',
+#                   'Roswell',
+#                   'Sandy Springs',
+#                   'Stone Mountain',
+#                   'Union City']
+
+  def full_street_address
+    "#{street_address}, #{city}, GA"
+  end
 end
-
-
-
-# == Schema Information
-#
-# Table name: locations
-#
-#  id             :integer          not null, primary key
-#  street_address :string
-#  city           :string
-#  latitude       :float
-#  longitude      :float
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#
